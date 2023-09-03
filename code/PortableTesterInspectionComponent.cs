c527c7433a36a853559fa7b83d4fb762d6dd98b5
end360 <5761763+end360@users.noreply.github.com>
2023-09-03 15:07:28 -0400

    Added UI for the tester. A simulated component is used to check if you are looking at the tester. I considered making a generic component that would just return whatever Entity you're looking at, but that's something that'd only be useful in the base game mode or a library.
    All Changes:
    
    - Added UI
    - Updated namespace to be more specific (end360.TTT). I didn't think it was necessary to make a specific namespace for the portable tester.
    - Added package reference to terrortown because apparently just targeting a game isn't enough to have it properly reference/compile.
    - Removed extraneous debug log messages
    - Changed animation hold type for weapon, still renders at feet but maybe I'll figure out how to actually put it in the hands soon.
    - Changed most ConVars to be replicated so client HUD works properly.

---
+++ code/PortableTesterInspectionComponent.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrorTown;
+
+namespace end360.TTT
+{
+    public class PortableTesterInspectionComponent : SimulatedComponent, ISingletonComponent
+    {
+        public PortableTesterPlaced? LookingAt { get; protected set; }
+        public override void Simulate(IClient cl)
+        {
+            if (!Game.IsClient) return;
+
+            var tr = Trace.Ray(Entity.AimRay, 65565).WithAnyTags("solid").Run();
+
+            if (tr.Hit && tr.Entity is PortableTesterPlaced tester)
+                LookingAt = tester;
+            else
+                LookingAt = null;
+        }
+
+        [Event("Player.PostSpawn")]
+        public static void PostSpawn(TerrorTown.Player ply) => ply.Components.Create<PortableTesterInspectionComponent>();
+    }
+}
